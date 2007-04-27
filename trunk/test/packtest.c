5c3c32536e22c6a34ef790a789ca5576d6b4abaf
ozzmosis <ozzmosis@c9eafb05-a8fa-e011-a987-0017a415b672>
2007-04-27 14:41:55 +0000

    printf %d requires an int.

--- trunk/test/packtest.c
+++ trunk/test/packtest.c
@@ -7,9 +7,9 @@
 
 int main(void)
 {
-    printf("sizeof(XMSG) == %d (should be %d)\n", sizeof(XMSG), 238);
+    printf("sizeof(XMSG) == %d (should be %d)\n", (int) sizeof(XMSG), 238);
     printf("sizeof(struct _stamp) == %d (should be %d)\n",
-      sizeof(struct _stamp), 4);
+      (int) sizeof(struct _stamp), 4);
     return 0;
 }
 
