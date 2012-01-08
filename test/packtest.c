54166785499840c40c7a78ea8e0032de762b2751
Andrew Clarke <28741388+zoomosis@users.noreply.github.com>
2012-01-08 16:09:47 +0000

    Cleanup

--- test/packtest.c
+++ test/packtest.c
@@ -5,11 +5,26 @@
 #include <stdio.h>
 #include "../src/msgapi.h"
 
-int main(void)
+static char *check_size(size_t size_have, size_t size_want)
 {
-    printf("sizeof(XMSG) == %d (should be %d)\n", (int) sizeof(XMSG), 238);
-    printf("sizeof(struct _stamp) == %d (should be %d)\n",
-      (int) sizeof(struct _stamp), 4);
-    return 0;
+    static char str[80];
+    
+    if (size_have == size_want)
+    {
+	sprintf(str, "%d (OK)", (int) size_have);
+    }
+    else
+    {
+	sprintf(str, "%d (Error: Should be %d!)", (int) size_have, (int) size_want);
+    }
+    
+    return str;
 }
 
+int main(void)
+{
+    printf("checking size of XMSG... %s\n", check_size(sizeof(XMSG), 238));
+    printf("checking size of struct _stamp... %s\n", check_size(sizeof(struct _stamp), 4));
+
+    return 0;
+}
